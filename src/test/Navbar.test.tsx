6df2f84e9973d224e625e6da0984d3d99aaf5f2e
aceade@github.com <philiprowlands90@gmail.com>
2023-10-12 09:52:33 +0100

    #15 opening submenus

--- src/test/Navbar.test.tsx
+++ src/test/Navbar.test.tsx
@@ -23,3 +23,19 @@ xit("Should be able to change the language - does not pick up language change ye
     fireEvent.click(screen.getByText("Gaeilge"));
     expect(screen.getByText("Gaeilge")).toHaveStyle("font-weight: bold;");
 });
+
+it("Clicking either submenu button should open that submenu", () => {
+    render(<MemoryRouter>
+        <Navbar/>
+    </MemoryRouter>);
+    const button = screen.getByText("navbar.see");
+    fireEvent.click(button);
+    const link = screen.getByText("navbar.colwdvatn");
+    expect(link).toBeVisible();
+
+
+    // the language buttons shouldn't visible until the change langauge icon is clicked
+    expect(screen.getByText("Gaeilge")).not.toBeVisible();
+    fireEvent.click(screen.getByAltText("navbar.changeLang.alt"));
+    expect(screen.getByText("Gaeilge")).toBeVisible();
+});
